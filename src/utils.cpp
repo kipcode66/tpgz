6557653e563a35d65715810f3a4e3f57594d0a63
jdflyer <yeacub@gmail.com>
2020-08-28 13:23:19 -0700

    Temporarily fixed the background music crash

--- src/utils.cpp
+++ src/utils.cpp
@@ -305,9 +305,9 @@ namespace Utilities {
     }
 
     void disable_bg_music() {
-        tp_zelAudio.bg_audio = 0.0f;
-        tp_zelAudio.enemy_bg_music_volume = 0.0f;
-        tp_zelAudio.hyrule_castle_bg_music_volume = 0.0f;
+        //tp_zelAudio.bg_audio = 0.0f;
+        //tp_zelAudio.enemy_bg_music_volume = 0.0f;
+        //tp_zelAudio.hyrule_castle_bg_music_volume = 0.0f;
     }
 
     void load_mem_card(MemCard::Card &card, SaveLayout &save_layout) {
@@ -326,9 +326,9 @@ namespace Utilities {
     }
 
     void enable_bg_music() {
-        tp_zelAudio.bg_audio = 1.0f;
-        tp_zelAudio.enemy_bg_music_volume = 1.0f;
-        tp_zelAudio.hyrule_castle_bg_music_volume = 1.0f;
+        //tp_zelAudio.bg_audio = 1.0f;
+        //tp_zelAudio.enemy_bg_music_volume = 1.0f;
+        //tp_zelAudio.hyrule_castle_bg_music_volume = 1.0f;
     }
 
     // void change_font(Font& font, const _Font& font_bytes, const char* font_texture_data ) {
