62269f4c1ce9c3946527c75758624ad07e856133
kipcode66 <egamer1010@gmail.com>
2020-09-07 01:05:02 -0400

    Updated inputs in descriptions

--- src/menus/tools_menu.cpp
+++ src/menus/tools_menu.cpp
@@ -43,8 +43,8 @@ Line lines[LINES] = {
     {"link debug info", LINK_DEBUG_INDEX, "Show Link's position, angle, and speed", true, &ToolItems[LINK_DEBUG_INDEX].active},
     {"no sinking in sand", SAND_INDEX, "Link won't sink in sand", true, &ToolItems[SAND_INDEX].active},
     {"roll checker", ROLL_INDEX, "Frame counter for chaining rolls", true, &ToolItems[ROLL_INDEX].active},
-    {"teleport", TELEPORT_INDEX, "dpadUp to set, dpadDown to load", true, &ToolItems[TELEPORT_INDEX].active},
-    {"timer", TIMER_INDEX, "Frame timer: Z+C+1 to start/stop, Z+C+2 to reset", true, &ToolItems[TIMER_INDEX].active},
+    {"teleport", TELEPORT_INDEX, "Z+C+1 to set, Z+C+2 to load", true, &ToolItems[TELEPORT_INDEX].active},
+    {"timer", TIMER_INDEX, "Frame timer: Z+C+A+B to start/stop, Z+C+B+Start to reset", true, &ToolItems[TIMER_INDEX].active},
     {"link tunic color:   ", TUNIC_COLOR_INDEX, "Changes Link's tunic color", false, nullptr, MAX_TUNIC_COLORS}};
 
 void ToolsMenu::render(Font& font) {
